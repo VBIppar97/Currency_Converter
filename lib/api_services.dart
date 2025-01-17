791628e077584a69d2503215d2a90c29d34cc181
Vaibhav Badrinath Ippar <121865234+VBIppar97@users.noreply.github.com>
2025-01-17 17:25:03 +0530

    Update api_services.dart

--- lib/api_services.dart
+++ lib/api_services.dart
@@ -2,32 +2,15 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  final String _apiKey = '0b787beced942186881bb4f2';
-  final String _baseUrl = 'https://v6.exchangerate-api.com/v6';
+  static const String _baseUrl =
+      "https://v6.exchangerate-api.com/v6/0b787beced942186881bb4f2/latest/USD";
 
-  // Fetch available currencies
-  Future<List<String>> getCurrencies(String baseCurrency) async {
-    final response = await http.get(Uri.parse('$_baseUrl/$_apiKey/latest/$baseCurrency'));
+  static Future<Map<String, dynamic>> fetchExchangeRates() async {
+    final response = await http.get(Uri.parse(_baseUrl));
     if (response.statusCode == 200) {
-      final data = json.decode(response.body);
-      return (data['conversion_rates'] as Map<String, dynamic>).keys.toList();
+      return json.decode(response.body)['conversion_rates'];
     } else {
-      throw Exception("Failed to fetch currencies");
-    }
-  }
-
-  // Convert currency
-  Future<double> convertCurrency(double amount, String fromCurrency, String toCurrency) async {
-    final response = await http.get(Uri.parse('$_baseUrl/$_apiKey/latest/$fromCurrency'));
-    if (response.statusCode == 200) {
-      final data = json.decode(response.body);
-      final exchangeRate = data['conversion_rates'][toCurrency];
-      if (exchangeRate == null) {
-        throw Exception("Currency not found");
-      }
-      return amount * exchangeRate;
-    } else {
-      throw Exception("Failed to convert currency");
+      throw Exception("Failed to load exchange rates");
     }
   }
 }
